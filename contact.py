40db522349f125c95e162fd99f8043d277352329
lsanopulos <lennysan_s@mail.ru>
2016-03-13 20:25:56 +0200

    Вспомогательные методы и классы для контакта

---
+++ contact.py
@@ -0,0 +1,22 @@
+class Contact:
+    def __init__(self, firstname, middlename, lastname, nickname, title, company, address, homePhone, mobilePhone,
+                 workPhone, fax, email, homepage, year, address2, phone2, notes):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.address = address
+        self.homePhone = homePhone
+        self.mobilePhone = mobilePhone
+        self.workPhone = workPhone
+        self.fax = fax
+        self.email = email
+        self.homepage = homepage
+        self.year = year
+        self.address2 = address2
+        self.phone2 = phone2
+        self.notes = notes
+
+
