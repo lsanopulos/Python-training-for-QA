2e1c5cb125e83c04ff4af9f385715f918421548e
lsanopulos <lennysan_s@mail.ru>
2016-05-17 16:20:37 +0200

    Подготовка allurex86

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -5,10 +5,13 @@ import pytest
 
 #@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
 def test_test_add_group(app, db, json_groups):
+    with pytest.allure.step('Given a group list'):
+        old_groups = db.get_group_list()
     group = json_groups
-    old_groups = db.get_group_list()
-    app.group.create(group)
+    with pytest.allure.step('When I add group %s to the list' % group):
+        app.group.create(group)
     #assert len(old_groups) + 1 == app.group.count()
-    new_groups = db.get_group_list()
-    old_groups.append(group)
-    assert sorted(old_groups, key = Group.id_or_max) == sorted(new_groups, key = Group.id_or_max)
+    with pytest.allure.step('Then the new group list is equal to the old list with the edit group'):
+        new_groups = db.get_group_list()
+        old_groups.append(group)
+        assert sorted(old_groups, key = Group.id_or_max) == sorted(new_groups, key = Group.id_or_max)
